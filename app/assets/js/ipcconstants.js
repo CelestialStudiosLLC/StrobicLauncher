2afdd720f55007f7f2744614cf875b9cfdb0cd13
Stefano <stefano@stefanocoding.me>
2024-02-10 11:48:55 +0100

    Updated links

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,8 +1,8 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
-//exports.AZURE_CLIENT_ID = '8ecafa18-8862-4860-86dd-6015636d67a7' -- Awaiting microsoft approval
+//exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '8ecafa18-8862-4860-86dd-6015636d67a7' //-- Awaiting microsoft approval
 
 // SEE NOTE ABOVE.
 
